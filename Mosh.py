f6eb9c4e508b3e6f3ddcbc4674987ac05d89ce93
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-07-06 21:24:36 +0600

    Update Mosh.py

--- Mosh.py
+++ Mosh.py
@@ -2,3 +2,38 @@
 
 print ("Hello World!")
 print ("Zizan")
+
+
+
+
+## 03. Variables
+
+
+## 03.01. Basic Syntax 
+
+price = 10
+print (price)
+
+## 03.02. Updatation
+
+price = 10 
+price = 20 
+print (price)
+
+
+## 03.03. Data types
+
+price = 10
+rating = 4.9 
+name = "Zizan"
+is_published = True
+print(price, rating, name, is_published)
+
+
+## 03.04. Exercise
+## Check a patient named John Smith, 20 years age and he's a new patient.
+
+name = "John Smith"
+age = 20
+new_patient = True 
+print(name, age, new_patient)
