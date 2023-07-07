a558777f53299cebff09f6d074ed3b1ed8f5cf40
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-07-07 12:14:21 +0600

    Update Mosh.py

--- Mosh.py
+++ Mosh.py
@@ -37,3 +37,42 @@ name = "John Smith"
 age = 20
 new_patient = True 
 print(name, age, new_patient)
+
+
+
+
+
+
+
+
+# 04. Getting inputs 
+name = input("What is your name?: ")
+favorite_color = input('What is your favorite color?: ')
+print(name + ' likes ' + favorite_color)
+
+
+
+
+
+
+
+
+
+# 05. Type conversion
+birth_year = input("birth year: ")
+age = 2023-int(birth_year)
+print(age)
+
+
+# 05.01. Identifying types 
+birth_year = input("birth_year: ")
+print(type(birth_year))
+age = 2023-int(birth_year)
+print(type(age))
+
+
+# 05.02. Identifying types (with conversion)
+birth_year = int(input("birth_year: "))
+print(type(birth_year))
+age = 2023-int(birth_year)
+print(type(age))
