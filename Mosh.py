88f6803dc336499b3da391b455cceb2208ec27bb
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-07-08 23:45:50 +0600

    Update Mosh.py

--- Mosh.py
+++ Mosh.py
@@ -218,3 +218,109 @@ else:
     down_payment = price*0.2
 
 print(f"down payment: {down_payment}") 
+
+
+
+
+
+
+
+
+
+
+# 12. IF Statements
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day.")
+    print("Drink plenty of water.")
+elif is_cold:
+    print("It's a cold day.")
+    print("Wear warm cloths.")
+else:
+    print("It's a lovely day!")
+print("Enjoy your day!")
+
+
+
+
+# 12.01. Exercise
+price = 1000000
+has_good_credit = True
+
+
+if has_good_credit:
+    down_payment = price*0.1
+else:
+    down_payment = price*0.2
+
+print(f"down payment: {down_payment}") 
+
+
+
+
+# 13. Logical operators (AND, OR, NOT)
+
+# 13.01. and operator
+has_good_income = True 
+good_credit_score = True
+
+if has_good_income and good_credit_score:
+    print("Eligible for loan!")
+else:
+    print("Not eligible for loan!")
+print("Thank you!")
+
+
+
+# 13.02. or operator
+has_good_income = True
+has_criminal_record = False
+
+
+if has_good_income or has_criminal_record:
+    print("Eligible for loan!")
+else:
+    print("Not eligible for loan!")
+
+
+
+# 13.03. not operator
+has_good_income = True 
+has_criminal_record = False 
+good_credit_score = True
+
+
+if has_good_income and not has_criminal_record and good_credit_score:
+    print("Eligible for loan!")
+else:
+    print("Not eligible for loan!")
+
+
+
+
+
+# 14. Comparison operator
+temperature = int(input("Enter the temperature: "))
+
+if temperature > 30 and temperature < 40:
+    print(f"Because of higher than {temperature} temperature, it's a hot day!")
+elif temperature < 10:
+    print(f"Because of less than {temperature} temperature, it's cold day!")
+elif temperature == 40:
+    print("It's too high temperature today! Stay safe!")
+else:
+    print("Just a normal day!")
+
+
+
+# 14.01. Exercise
+name = input("What's your name: ")
+
+if len(name) < 3:
+    print("Name must be 3 characters long!")
+elif len(name) > 50:
+    print("Name must be 50 characters long!")
+else:
+    print("Name is Ok!")
